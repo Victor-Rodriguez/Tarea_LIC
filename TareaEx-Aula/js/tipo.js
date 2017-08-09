ddab520739a8f603d66eec99d45077faebbd11fa
edwinmejia-123 <30848466+edwinmejia-123@users.noreply.github.com>
2017-08-09 02:26:05 -0600

    Update tipo.js

--- TareaEx-Aula/js/tipo.js
+++ TareaEx-Aula/js/tipo.js
@@ -2,7 +2,7 @@ var a = Number(prompt('Ingrese el lado 1 del triangulo',''));
 var b = Number(prompt('Ingrese el lado 2 del triangulo',''));
 var c = Number(prompt('Ingrese el lado 3 del triangulo',''));
 var s = ((a+b+c)/2) ;
-var area = (Math.pow(s* (s-a) * (s-b) * (s-c)),(1/2));
+var area = Math.pow((s * (s - a) * (s - b) * (s - c)), (1 / 2));
 if (a==b && a==c && b==c) 
 {
 	document.write("<img src='../img/equilatero.jpg'>");
