66816aa11314f0e83706a4da9868547fd3a7a4d7
edwinmejia-123 <30848466+edwinmejia-123@users.noreply.github.com>
2017-08-08 23:29:41 -0600

    Update tipo.js

--- js/tipo.js
+++ js/tipo.js
@@ -1,13 +1,13 @@
-var a = prompt('Ingrese el lado 1 del triangulo','');
-var b = prompt('Ingrese el lado 2 del triangulo','');
-var c = prompt('Ingrese el lado 3 del triangulo','');
-var s = ((a+b+c)/2) 
-var area = (Math.pow(s* (s-a) * (s-b) * (s-c)),(1/2))
-if (a = b && a = c && b = c)
+var a = Number(prompt('Ingrese el lado 1 del triangulo',''));
+var b = Number(prompt('Ingrese el lado 2 del triangulo',''));
+var c = Number(prompt('Ingrese el lado 3 del triangulo',''));
+var s = ((a+b+c)/2) ;
+var area = (Math.pow(s* (s-a) * (s-b) * (s-c)),(1/2));
+if (a==b && a==c && b==c) 
 {
-	document.write();
-}else if (a = b || a = c || b = c) {
-	document.write();
+	document.write('<img src="../img/equilatero.jpg">');
+}else if (a == b || a == c || b == c) {
+	document.write('<img src="../img/isosceles.jpg">');
 }else if (a !=b && a!=c && b!=c) {
-	document.write();
+	document.write('<img src="../img/escaleno.jpg">');
 }
