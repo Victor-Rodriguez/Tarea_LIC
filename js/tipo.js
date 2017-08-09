39a12850e22bb9e6adc5e358f8075ae5363b4cdc
edwinmejia-123 <30848466+edwinmejia-123@users.noreply.github.com>
2017-08-08 22:49:32 -0600

    Create tipo.js

---
+++ js/tipo.js
@@ -0,0 +1,13 @@
+var a = prompt('Ingrese el lado 1 del triangulo','');
+var b = prompt('Ingrese el lado 2 del triangulo','');
+var c = prompt('Ingrese el lado 3 del triangulo','');
+var s = ((a+b+c)/2) 
+var area = (Math.pow(s* (s-a) * (s-b) * (s-c)),(1/2))
+if (a = b && a = c && b = c)
+{
+	document.write();
+}else if (a = b || a = c || b = c) {
+	document.write();
+}else if (a !=b && a!=c && b!=c) {
+	document.write();
+}
